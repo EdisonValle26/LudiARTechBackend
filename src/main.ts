d477f72733b4e299e3853544b1a59bfdf977ac45
EdisonValle26 <andres.valle150@gmail.com>
2026-01-06 22:17:21 -0500

    fix host y puerto

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(process.env.PORT ?? 3000, configService.get<string>('SERVER_HOST') ?? 'localhost');
+  await app.listen(process.env.PORT ?? 10000, process.env.SERVER_HOST ?? '0.0.0.0');
 }
 
 bootstrap()
